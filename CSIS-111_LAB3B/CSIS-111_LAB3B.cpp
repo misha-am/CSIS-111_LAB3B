462a9e55885f1558208d7106e6b8add9a9596e0b
Michael Ubinas <mjubinas@liberty.edu>
2021-04-19 14:41:42 -0400

    Add project files.

---
+++ CSIS-111_LAB3B/CSIS-111_LAB3B.cpp
@@ -0,0 +1,26 @@
+// Michael Ubinas
+// C++ Program to illustrate the iterators in vector
+// https://www.geeksforgeeks.org/vector-in-cpp-stl/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> g1;
+
+	for (int i = 1; i <= 5; i++)
+		g1.push_back(i);
+
+	cout << "Numbers in vector: ";
+	for (int i = 0; i < g1.size(); i++)
+		cout << g1[i] << " ";
+
+	cout << "\n\nNumbers in reverse:";
+	for (int i = g1.size() - 1; i >= 0; i--)
+		cout << g1[i] << " ";
+
+	cout << "\n\nEnd of Program\n\n";
+	system("pause");
+	return 0;
+}
